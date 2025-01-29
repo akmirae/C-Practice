c4ee020c180d291add8b37e5642e5e13869c9573
akmirae <akmirae@gmail.com>
2025-01-28 19:09:18 -0500

    01/28/2025

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// 프로그램을 작성하여 "Hello, World!" 를 출력하세요.
+
+
+
+#include <iostream> //입력과 출력을 위한 헤더 파일
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // 화면에 메세지 출력
+    return 0; // 프로그램 정상종료
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //입력과 출력을 위한 헤더 파일
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // 화면에 메세지 출력
+    return 0; // 프로그램 정상종료
+}
