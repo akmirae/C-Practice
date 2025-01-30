0c305a125e5a58e4e38f91d14ae46ff4dda17173
akmirae <akmirae@gmail.com>
2025-01-30 11:07:33 -0500

    01302025

---
+++ 2.4reverse_string.cpp
@@ -0,0 +1,7 @@
+// Reverse a string
+// Write a program that reverses the string entered by the user and outputs it.
+// Example: "hello" → "olleh"
+
+// 문자열 뒤집기
+// 사용자가 입력한 문자열을 뒤집어 출력하는 프로그램을 작성하세요.
+// 예: "hello" → "olleh"
